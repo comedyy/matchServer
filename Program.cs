3417f5d774f3cf370e91b54ce02c7c5bffdd01c4
张敦勇 <dunyong.zhang@igg.com>
2024-01-23 09:50:33 +0800

    添加房间id配置。

--- Program.cs
+++ Program.cs
@@ -12,10 +12,12 @@ class Program
     static Thread _thread;
     static bool NeedStop;
     static int mainThreadSleepTime;
+    const string appConfigFolder = "__appconfig";
 
     static void Main(string[] args)
     {
         Init();
+        CreaetAppConfigFolder();
         // NetMgr.Instance.Init();
 
         ThreadStart threadStart = ProcessLogic;
@@ -37,6 +39,14 @@ class Program
         }
 
         Console.WriteLine(" ---------------server end------------------ ");
+    }
+
+    private static void CreaetAppConfigFolder()
+    {
+        if(!Directory.Exists(appConfigFolder))
+        {
+            Directory.CreateDirectory(appConfigFolder);
+        }
     }
 
     private static void ProcessGM(string line)
@@ -103,25 +113,77 @@ class Program
 
             msTick = watch.ElapsedMilliseconds - msEndOfFrame;
             msEndOfFrame = watch.ElapsedMilliseconds;
+
+            if(frame % 1000 == 0 && _netProcessor != null)
+            {
+                try
+                {
+                    File.WriteAllText(roomTxt, _netProcessor.RoomId.ToString());
+                }
+                catch(Exception e)
+                {
+                    // nothing
+                    Console.WriteLine("写入房间id异常" + e.Message);
+                }
+            }
         }
     }
 
     static Dictionary<string, int> _dicConfigs = new Dictionary<string, int>();
     private static void Init()
     {
-        var lines = File.ReadAllLines("appConfig.txt");
-        foreach(var x in lines)
+        var configPath = $"{appConfigFolder}/appConfig.txt";
+        if(File.Exists(configPath))
         {
-            var args = x.Split(",", StringSplitOptions.RemoveEmptyEntries);
-            if(args.Length < 2) continue;
+            var lines = File.ReadAllLines(configPath);
+            foreach(var x in lines)
+            {
+                var args = x.Split(",", StringSplitOptions.RemoveEmptyEntries);
+                if(args.Length < 2) continue;
 
-            _dicConfigs[args[0]] = int.Parse(args[1]);
+                _dicConfigs[args[0]] = int.Parse(args[1]);
 
-            Console.WriteLine($"Load {x}");
+                Console.WriteLine($"Load {x}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("未找到appConfig.txt");
         }
 
         int port;
         if (!_dicConfigs.TryGetValue("port", out port)) port = 5000;
-        _netProcessor = new NetProcessor(new GameServerSocket(1000, port));
+
+        var initRoomId = GetInitRoomId();
+        _netProcessor = new NetProcessor(new GameServerSocket(1000, port), initRoomId);
+    }
+
+    static string roomTxt = $"{appConfigFolder}/roomId.txt";
+    private static int GetInitRoomId()
+    {
+        try
+        {
+            if(!File.Exists(roomTxt))
+            {
+                Console.WriteLine("未找到房间id配置，初始0");
+                return 0;
+            }
+
+            var id = int.Parse(File.ReadAllText(roomTxt));
+            var targetId = id + 10000;
+
+            if(targetId < 0)
+            {
+                targetId = 100;
+            }
+
+            Console.WriteLine($"读取到房间配置：{id}, 更新房间初始id{targetId}");
+            return targetId;
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine("读取房间id异常"+ e.Message);
+            return 0;
+        }
     }
 }
